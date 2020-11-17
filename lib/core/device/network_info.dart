9374b4bd128191e3d9901da253b9725ea7356462
Daniel Cunha <danielcunha54@gmail.com>
2020-11-16 21:37:35 -0300

    feat: create NetworkInfo

---
+++ lib/core/device/network_info.dart
@@ -0,0 +1,16 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+@LazySingleton(as: NetworkInfo)
+class NetworkInfoImpl implements NetworkInfo {
+  final DataConnectionChecker connectionChecker;
+
+  const NetworkInfoImpl(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}
