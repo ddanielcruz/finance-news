c7c88bd6ce500c7bc3c1c66b060474fd29023645
Daniel Cunha <danielcunha54@gmail.com>
2020-11-16 21:21:00 -0300

    feat: implement CategoryLocalDataSource

--- lib/features/news/data/datasources/category_local_data_source.dart
+++ lib/features/news/data/datasources/category_local_data_source.dart
@@ -1,5 +1,47 @@
 import 'package:finance_news/features/news/domain/entities/category.dart';
+import 'package:injectable/injectable.dart';
 
 abstract class CategoryLocalDataSource {
   List<Category> getAll();
+}
+
+@LazySingleton(as: CategoryLocalDataSource)
+class CategoryLocalDataSourceImpl implements CategoryLocalDataSource {
+  @override
+  List<Category> getAll() {
+    return const [
+      Category(
+        name: 'Últimas',
+        url: 'https://www.infomoney.com.br/feed',
+      ),
+      Category(
+        name: 'Mercados',
+        url: 'https://www.infomoney.com.br/mercados/feed',
+      ),
+      Category(
+        name: 'Finanças',
+        url: 'https://www.infomoney.com.br/minhas-financas/feed',
+      ),
+      Category(
+        name: 'Política',
+        url: 'https://www.infomoney.com.br/politica/feed',
+      ),
+      Category(
+        name: 'Negócios',
+        url: 'https://www.infomoney.com.br/negocios/feed',
+      ),
+      Category(
+        name: 'Consumo',
+        url: 'https://www.infomoney.com.br/consumo/feed',
+      ),
+      Category(
+        name: 'Carreira',
+        url: 'https://www.infomoney.com.br/carreira/feed',
+      ),
+      Category(
+        name: 'Economia',
+        url: 'https://www.infomoney.com.br/economia/feed',
+      ),
+    ];
+  }
 }
